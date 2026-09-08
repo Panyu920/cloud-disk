3894df3596c7e2ad88aaf32ef7c8fd23ed5d2f2b
panyu <936253519@gmail.com>
2026-09-08 08:30:08 +0000

    添加mysql的dbml文件

--- Makefile
+++ Makefile
@@ -9,3 +9,9 @@ remove:
 
 setup-mysql-master-slave:
 	@chmod +x ./scripts/setup_mysql_master_slave.sh && ./scripts/setup_mysql_master_slave.sh $(PASSWORD) $(MASTER_HOST)
+
+db_docs:
+	dbdocs build ./doc/db.dbml
+
+db_schema:
+	dbml2sql ./doc/db.dbml -o ./doc/schema.sql --mysql
---
+++ doc/schema.sql
@@ -0,0 +1,22 @@
+-- SQL dump generated using DBML (dbml.dbdiagram.io)
+-- Database: MySQL
+-- Generated at: 2026-09-08T08:24:32.774Z
+
+CREATE TABLE `files` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `file_sha1` varchar(255) UNIQUE NOT NULL COMMENT '文件的sha1值',
+  `file_name` varchar(255) NOT NULL COMMENT '文件名',
+  `file_size` bigint NOT NULL DEFAULT 0 COMMENT '文件大小',
+  `file_addr` varchar(1024) NOT NULL DEFAULT '' COMMENT '文件存储地址',
+  `status` int NOT NULL DEFAULT 0 COMMENT '文件状态(0: 正常,1:禁用, 2: 删除)',
+  `create_at` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT '创建时间',
+  `update_at` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT '更新时间',
+  `extend` int NOT NULL DEFAULT 0 COMMENT '扩展字段',
+  `extend2` text NOT NULL DEFAULT '' COMMENT '扩展字段2'
+);
+
+CREATE INDEX `idx_status` ON `files` (`status`);
+
+CREATE UNIQUE INDEX `idx_file_sha1` ON `files` (`file_sha1`);
+
+ALTER TABLE `files` COMMENT = '文件表';
